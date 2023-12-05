1958c61474cd8c572f7bf42f32a33587cb4184d8
Anith Vishwanath <anithvishwanath@Aniths-MacBook-Pro.local>
2023-12-05 12:49:28 +0530

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Anith Vishwanath</title>
+	<meta name="description" content="Anith Vishwanath is a digital designer focusing on interaction design and design systems." />
+</svelte:head>
+
+<section>
+	<h1>
+		<!-- <span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span> -->
+		Anith Vishwanath
+	</h1>
+
+	<p>I craft scalable and user-focused experiences that help businesses grow and thrive, and I transform abstract ideas into tangible products that are both functional and delightful to use.</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
